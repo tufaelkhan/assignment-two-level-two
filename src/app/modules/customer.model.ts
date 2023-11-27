bb98cb5748e8f4a4fb57044ba252695f5703e6f4
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-27 15:30:48 -0800

    add model

--- src/app/modules/customer.model.ts
+++ src/app/modules/customer.model.ts
@@ -1,5 +1,7 @@
 import { Schema, model, } from 'mongoose';
 import { TCustomer, TOrders } from './customer/customer.interface';
+import bcrypt from 'bcrypt'
+import config from '../config';
 
 const orderSchema = new Schema<TOrders>({
     productName: { type: String, required: true},
@@ -54,5 +56,13 @@ const customerSchema = new Schema<TCustomer>({
         default: []
     }
 })
+
+customerSchema.pre('save', async function(next){
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const user = this
+    user.password = await bcrypt.hash(user.password, Number(config.bcrypt_salt_rounds));
+    next()
+})
+
 
 export const CustomerModel = model<TCustomer>('Customer', customerSchema)
